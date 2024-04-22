7d7755a90525b9af32b2f83d54661ca86701566b
Ramadhan Purbo Sejati <ramadhan.purbo@akarinti.tech>
2024-04-22 10:05:23 +0700

    feat: init core

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+import org.jetbrains.compose.ExperimentalComposeLibrary
+
+plugins {
+    alias(libs.plugins.multiplatform)
+    alias(libs.plugins.compose)
+    alias(libs.plugins.buildConfig)
+    alias(libs.plugins.kotlinx.serialization)
+}
+
+val packageName = "app.dayacore.pos"
+
+kotlin {
+    jvm()
+
+    sourceSets {
+        all {
+            languageSettings {
+                optIn("org.jetbrains.compose.resources.ExperimentalResourceApi")
+            }
+        }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.voyager.navigator)
+            implementation(libs.composeImageLoader)
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kotlinx.datetime)
+            implementation(libs.koin.core)
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.uiTest)
+            implementation(libs.kotlinx.coroutines.test)
+        }
+
+    }
+}
+
+buildConfig {
+    // BuildConfig configuration here.
+    // https://github.com/gmazzo/gradle-buildconfig-plugin#usage-in-kts
+    forClass("SyncType") {
+        packageName("$packageName")
+
+        buildConfigField("String", "SYNC_PRODUCT", "\"sync_product\"")
+        buildConfigField("String", "SYNC_ORDER", "\"sync_order\"")
+    }
+}
